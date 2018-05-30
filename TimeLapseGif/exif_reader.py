84caa109da6d22062e8f5d9c387ed28c238d015f
Jacob Long <36164786+Jclong98@users.noreply.github.com>
2018-05-29 22:58:22 -0700

    Add files via upload

--- TimeLapseGif/exif_reader.py
+++ TimeLapseGif/exif_reader.py
@@ -3,7 +3,6 @@ import PIL.Image
 import tkinter.filedialog as fd
 
 img_list = []
-exif_list = []
 
 def get_images():
     # asking where the images are stored
@@ -17,15 +16,16 @@ def get_images():
         img = PIL.Image.open(path + "/" + filename)
         exif_data = img._getexif()
 
-        img_list.append(filename)
-        exif_list.append(exif_data)
+        # this creates a dictionary for each image so that they can be accessed by ['image_name'] or ['exif']
+        img_list.append({'image_name':filename, 'exif':exif_data})
+        
 
 if __name__ == '__main__':
     get_images()
     
     # this prints the first dictionary of exif data from the exif list
-    # print(exif_list[0])
-
+    # print(img_list[0]['exif'])
+    
     # printing out each image name, associated with the date it was taken from the exif data
-    for i in range(len(img_list)):
-        print(img_list[i] + "\t" + exif_list[i][306])
+    for image in img_list:
+        print(image['image_name'] + "\t" + image['exif'][306])
