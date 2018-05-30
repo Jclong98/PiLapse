34faeea64dd70a5b40b3f2e8b11273d1f708e42f
Jacob Long <36164786+Jclong98@users.noreply.github.com>
2018-05-29 21:24:26 -0700

    Add files via upload

---
+++ TimeLapseGif/gifmaker.py
@@ -0,0 +1,17 @@
+import os
+import imageio as io
+import tkinter.filedialog as fd
+
+def create_gif():
+    root = fd.Tk()
+    root.withdraw()
+    path = fd.askdirectory()
+
+    with io.get_writer(path + "/" + 'PyLapse.gif', mode='I', duration=0.15) as writer:
+        for filename in os.listdir(path):
+            image = io.imread(path + "/" + filename)
+            writer.append_data(image)
+    writer.close()
+
+if __name__ == '__main__':
+    create_gif()
